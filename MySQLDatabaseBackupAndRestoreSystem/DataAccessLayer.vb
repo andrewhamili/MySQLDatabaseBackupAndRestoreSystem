2d905a9eb5e0f620e74c073a6b749741a059fce2
andrewhamili <rockr1521@gmail.com>
2017-06-06 21:46:02 +0800

    Prepare for Telerik WinForms

---
+++ MySQLDatabaseBackupAndRestoreSystem/DataAccessLayer.vb
@@ -0,0 +1,74 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class DataAccessLayer
+    Protected MySQLConn As New MySqlConnection
+    Protected db As String = ""
+    Protected server As String = ""
+    Protected uname As String = ""
+    Protected pwd As String = ""
+    Public Sub New(Host As String, Database As String, Username As String, Password As String)
+        server = Host
+        db = Database
+        uname = Username
+        pwd = Password
+    End Sub
+    Protected Sub ConnectDB()
+        With MySQLConn
+            .ConnectionString = String.Format("server={0};username={1};password={2};database={3}", server, uname, pwd, db)
+            .Open()
+        End With
+    End Sub
+    Protected Sub CloseDB()
+        With MySQLConn
+            .Close()
+        End With
+    End Sub
+    Public Function ExecQuery(ByVal query As String) As DataTable
+        Return ExecCommand(query)
+    End Function
+    Public Function ExecStoredProc(ByVal storedProc As String, Optional ByVal param As List(Of MySqlParameter) = Nothing) As DataTable
+        Return ExecCommand(storedProc, True, param)
+    End Function
+    Private Function ExecCommand(ByVal query As String, Optional ByVal useStoredProc As Boolean = False, Optional ByVal param As List(Of MySqlParameter) = Nothing) As DataTable
+        ConnectDB()
+        Dim adapter As New MySqlDataAdapter
+        Dim comm As New MySqlCommand
+        Dim dbdataset As New DataTable
+
+        With comm
+
+            If Not userStoredProc Then
+                .CommandType = CommandType.Text
+            Else
+                .CommandType = CommandType.StoredProcedure
+                If Not IsNothing(param) Then
+                    .Parameters.Clear()
+                    For Each sqlParam As MySqlParameter In param
+                        .Parameters.Add(sqlParam)
+                    Next
+                End If
+            End If
+        End With
+        With adapter
+            .SelectCommand = comm
+            .Fill(dbdataset)
+            .Dispose()
+        End With
+        CloseDB()
+        Return dbdataset
+    End Function
+    Public Function ExecScalar(ByVal query As String) As Object
+        Dim result As Object
+        ConnectDB()
+        Dim comm As New MySqlCommand
+        With comm
+            .Connection = MySQLConn
+            .CommandText = query
+            .CommandType = CommandType.Text
+
+            result = .ExecuteScalar
+        End With
+
+        CloseDB()
+        Return result
+    End Function
+End Class
---
+++ MySQLDatabaseBackupAndRestoreSystem/Main.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits Telerik.WinControls.UI.RadForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.VisualStudio2012DarkTheme1 = New Telerik.WinControls.Themes.VisualStudio2012DarkTheme()
+        Me.RadLabel1 = New Telerik.WinControls.UI.RadLabel()
+        Me.RadLabel2 = New Telerik.WinControls.UI.RadLabel()
+        Me.RadLabel3 = New Telerik.WinControls.UI.RadLabel()
+        Me.RadLabel4 = New Telerik.WinControls.UI.RadLabel()
+        Me.RadTextBox1 = New Telerik.WinControls.UI.RadTextBox()
+        Me.RadTextBox2 = New Telerik.WinControls.UI.RadTextBox()
+        Me.RadTextBox3 = New Telerik.WinControls.UI.RadTextBox()
+        Me.RadTextBox4 = New Telerik.WinControls.UI.RadTextBox()
+        Me.RadButton1 = New Telerik.WinControls.UI.RadButton()
+        CType(Me.RadLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadTextBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadTextBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadTextBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadTextBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadButton1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RadLabel1
+        '
+        Me.RadLabel1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.RadLabel1.Location = New System.Drawing.Point(13, 55)
+        Me.RadLabel1.Name = "RadLabel1"
+        Me.RadLabel1.Size = New System.Drawing.Size(183, 31)
+        Me.RadLabel1.TabIndex = 0
+        Me.RadLabel1.Text = "Server / Hostname:"
+        Me.RadLabel1.ThemeName = "VisualStudio2012Dark"
+        '
+        'RadLabel2
+        '
+        Me.RadLabel2.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.RadLabel2.Location = New System.Drawing.Point(131, 101)
+        Me.RadLabel2.Name = "RadLabel2"
+        Me.RadLabel2.Size = New System.Drawing.Size(51, 31)
+        Me.RadLabel2.TabIndex = 1
+        Me.RadLabel2.Text = "Port:"
+        Me.RadLabel2.ThemeName = "VisualStudio2012Dark"
+        '
+        'RadLabel3
+        '
+        Me.RadLabel3.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.RadLabel3.Location = New System.Drawing.Point(87, 144)
+        Me.RadLabel3.Name = "RadLabel3"
+        Me.RadLabel3.Size = New System.Drawing.Size(105, 31)
+        Me.RadLabel3.TabIndex = 2
+        Me.RadLabel3.Text = "Username:"
+        Me.RadLabel3.ThemeName = "VisualStudio2012Dark"
+        '
+        'RadLabel4
+        '
+        Me.RadLabel4.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.RadLabel4.Location = New System.Drawing.Point(87, 200)
+        Me.RadLabel4.Name = "RadLabel4"
+        Me.RadLabel4.Size = New System.Drawing.Size(99, 31)
+        Me.RadLabel4.TabIndex = 3
+        Me.RadLabel4.Text = "Password:"
+        Me.RadLabel4.ThemeName = "VisualStudio2012Dark"
+        '
+        'RadTextBox1
+        '
+        Me.RadTextBox1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.RadTextBox1.Location = New System.Drawing.Point(198, 55)
+        Me.RadTextBox1.Name = "RadTextBox1"
+        Me.RadTextBox1.Size = New System.Drawing.Size(169, 33)
+        Me.RadTextBox1.TabIndex = 4
+        Me.RadTextBox1.ThemeName = "VisualStudio2012Dark"
+        '
+        'RadTextBox2
+        '
+        Me.RadTextBox2.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.RadTextBox2.Location = New System.Drawing.Point(198, 101)
+        Me.RadTextBox2.Name = "RadTextBox2"
+        Me.RadTextBox2.Size = New System.Drawing.Size(169, 33)
+        Me.RadTextBox2.TabIndex = 5
+        Me.RadTextBox2.ThemeName = "VisualStudio2012Dark"
+        '
+        'RadTextBox3
+        '
+        Me.RadTextBox3.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.RadTextBox3.Location = New System.Drawing.Point(198, 144)
+        Me.RadTextBox3.Name = "RadTextBox3"
+        Me.RadTextBox3.Size = New System.Drawing.Size(169, 33)
+        Me.RadTextBox3.TabIndex = 6
+        Me.RadTextBox3.ThemeName = "VisualStudio2012Dark"
+        '
+        'RadTextBox4
+        '
+        Me.RadTextBox4.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.RadTextBox4.Location = New System.Drawing.Point(198, 200)
+        Me.RadTextBox4.Name = "RadTextBox4"
+        Me.RadTextBox4.Size = New System.Drawing.Size(169, 33)
+        Me.RadTextBox4.TabIndex = 7
+        Me.RadTextBox4.ThemeName = "VisualStudio2012Dark"
+        '
+        'RadButton1
+        '
+        Me.RadButton1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.RadButton1.Location = New System.Drawing.Point(140, 264)
+        Me.RadButton1.Name = "RadButton1"
+        Me.RadButton1.Size = New System.Drawing.Size(155, 50)
+        Me.RadButton1.TabIndex = 8
+        Me.RadButton1.Text = "Connect"
+        Me.RadButton1.ThemeName = "VisualStudio2012Dark"
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(379, 368)
+        Me.Controls.Add(Me.RadButton1)
+        Me.Controls.Add(Me.RadTextBox4)
+        Me.Controls.Add(Me.RadTextBox3)
+        Me.Controls.Add(Me.RadTextBox2)
+        Me.Controls.Add(Me.RadTextBox1)
+        Me.Controls.Add(Me.RadLabel4)
+        Me.Controls.Add(Me.RadLabel3)
+        Me.Controls.Add(Me.RadLabel2)
+        Me.Controls.Add(Me.RadLabel1)
+        Me.Name = "Main"
+        '
+        '
+        '
+        Me.RootElement.ApplyShapeToControl = True
+        Me.Text = "Main"
+        Me.ThemeName = "VisualStudio2012Dark"
+        CType(Me.RadLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadTextBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadTextBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadTextBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadTextBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadButton1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents VisualStudio2012DarkTheme1 As Telerik.WinControls.Themes.VisualStudio2012DarkTheme
+    Friend WithEvents RadLabel1 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents RadLabel2 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents RadLabel3 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents RadLabel4 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents RadTextBox1 As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents RadTextBox2 As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents RadTextBox3 As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents RadTextBox4 As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents RadButton1 As Telerik.WinControls.UI.RadButton
+End Class
+
